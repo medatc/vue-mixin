82679f47c86d45057d0f1d401b269f1a106f7c32
lzxb <1340641314@qq.com>
2017-04-17 16:57:02 +0800

    test:vue-mixin-store disable update

--- test/unit/vue-mixin-store.test.js
+++ test/unit/vue-mixin-store.test.js
@@ -58,4 +58,61 @@ describe('vue-mixin-store', () => {
       })
     })
   })
+  describe('disable update', () => {
+    const { store } = new VueMixin({
+      plugins: [
+        vueMixinStore({
+          user: {
+            update: false,
+            fetch () {
+              return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  resolve({
+                    id: 1000,
+                    age: 18,
+                    name: '人民的名义'
+                  })
+                }, 200)
+              })
+            }
+          }
+        })
+      ]
+    }).mixins
+    Object.assign(store, {
+      data () {
+        return {
+          user: {}
+        }
+      },
+      render (h) {
+        return h('div')
+      }
+    })
+    it('fetch data', () => {
+      return new Promise((resolve, reject) => {
+        const Constructor = Vue.extend(store)
+        const vm = new Constructor().$mount()
+        vm.$watch('user', (newVal, oldVal) => {
+          expect(newVal.id).to.be.equal(1000)
+          vm.$destroy()
+          resolve()
+        })
+      })
+    })
+    it('fetch update data', () => {
+      return new Promise((resolve, reject) => {
+        const Constructor = Vue.extend(store)
+        const vm = new Constructor().$mount()
+        vm.$watch('user', (newVal, oldVal) => {
+          vm.$destroy()
+          reject(new Error({ msg: 'update is expected to be prohibited' }))
+        })
+        setTimeout(() => {
+          vm.$destroy()
+          resolve()
+        }, 300)
+      })
+    })
+  })
 })
